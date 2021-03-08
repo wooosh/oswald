c7e1794011f2e01a74b0903a726ab6d55abb0465
wooosh <12696312+wooosh@users.noreply.github.com>
2021-03-08 16:15:39 -0500

    full file tree-sitter

--- src/modules/highlight_treesitter.cxx
+++ src/modules/highlight_treesitter.cxx
@@ -10,17 +10,20 @@ extern "C" {
 }
 
 static std::map<std::list<Buffer>::iterator, TSParser*> parsers;
-/*
-const char* treeSitterInput(std::list<Buffer>::iterator *buffer, uint32_t byte_offset, TSPoint pos, uint32_t *bytes_read) {
-  Buffer buf = **buffer;
-  if (pos.row < buf.rows.length()) {
-    *bytes_read = buf.rows[pos.row].length() - pos.column;
-    return buf.rows[pos.row].c_str() + pos.column;
+
+const char* treeSitterInput(void *buffer, uint32_t byte_offset, TSPoint pos, uint32_t *bytes_read) {
+  Buffer buf = **((std::list<Buffer>::iterator*) buffer);
+  if (pos.row < buf.rows.size()) {
+    *bytes_read = buf.rows[pos.row].raw.length() + 1 - pos.column;
+    // TODO: free this
+    static std::string row;
+    row = buf.rows[pos.row].raw + "\n";
+    return row.c_str() + pos.column;
   }
   *bytes_read = 0;
   return NULL;
 }
-*/
+
 
 void treeSitterOpenBuffer(std::list<Buffer>::iterator buffer) {
 
@@ -33,8 +36,12 @@ bool cursorNext(TSTreeCursor* cur) {
     return true;
   } else if (ts_tree_cursor_goto_next_sibling(cur)) {
     return true;
-  } else if (ts_tree_cursor_goto_parent(cur) && ts_tree_cursor_goto_next_sibling(cur)) {
-    return true;
+  } else {
+    while (ts_tree_cursor_goto_parent(cur)) {
+      if (ts_tree_cursor_goto_next_sibling(cur)) {
+        return true;
+      }
+    }
   }
   return false;
 }
@@ -47,12 +54,18 @@ void highlightTreeSitter(BufferEditEvent ev) {
   TSParser *parser = ts_parser_new();
   ts_parser_set_language(parser, tree_sitter_c());
 
-  TSTree *tree = ts_parser_parse_string(
+  TSTree *tree = ts_parser_parse(
     parser,
     NULL,
-    ev.buffer->rows[ev.line].raw.c_str(),
-    ev.buffer->rows[ev.line].raw.length()
+    {
+      (void*) &ev.buffer,
+      treeSitterInput,
+      TSInputEncodingUTF8
+    }
+    //ev.buffer->rows[ev.line].raw.c_str(),
+    //ev.buffer->rows[ev.line].raw.length()
   );
+
 
   TSTreeCursor cur = ts_tree_cursor_new(ts_tree_root_node(tree));
 
@@ -64,16 +77,15 @@ void highlightTreeSitter(BufferEditEvent ev) {
   while (cursorNext(&cur)) {
     TSNode node = ts_tree_cursor_current_node(&cur);
     if (strcmp(ts_node_type(node), "primitive_type") == 0 ||
-      strcmp(ts_node_type(node), "struct_specifier") == 0 ||
       strcmp(ts_node_type(node), "type_identifier") == 0
       ) {
       // we assume the highlight is only on one line currently
-      uint32_t start = ts_node_start_point(node).column;
-      uint32_t end = ts_node_end_point(node).column;
+      TSPoint start = ts_node_start_point(node);
+      TSPoint end = ts_node_end_point(node);
 
-      for (uint32_t i = start; i<end; i++) {
+      for (uint32_t i = start.column; i<end.column; i++) {
         
-        ev.buffer->rows[ev.line].hl[i] = HighlightType::Keyword;
+        ev.buffer->rows[start.row].hl[i] = HighlightType::Keyword;
       }
 
     }
